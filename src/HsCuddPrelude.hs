e875dbc57d8750fd36cd86127d125dfb539bb353
Bradford Larsen <brad.larsen@gmail.com>
2012-10-20 14:14:08 -0400

    Export more functions in HsCuddPrelude.

--- src/HsCuddPrelude.hs
+++ src/HsCuddPrelude.hs
@@ -2,8 +2,14 @@ module HsCuddPrelude (
     module Control.Applicative
   , module Control.Monad
   , module Data.List
+  , module Data.Maybe
+  , module Data.Ord
   , module Data.Typeable
   , module Data.Word
+
+  , module System.Exit
+  , module System.IO
+  , module Text.Printf
   ) where
 
 import Control.Applicative (
@@ -14,8 +20,15 @@ import Control.Monad (
   , foldM
   , join, guard
   , (>=>), (<=<)
+  , void
   , when, unless
   )
-import Data.List (sort)
+import Data.List (lookup, delete, splitAt, sort, sortBy)
+import Data.Maybe (fromJust, isJust, isNothing)
+import Data.Ord (comparing)
 import Data.Typeable (Typeable)
 import Data.Word (Word)
+
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (stdout, stderr, hFlush)
+import Text.Printf (printf, hPrintf)
